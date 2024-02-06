4846856c531fc747e37f8db7490c77cbdf51ce20
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-02-06 21:13:43 +0100

    configuración

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <main className="max-w-screen-sm mx-auto mt-10 px-4">
+      <h1 className="mb-10 text-3xl font-black">Vite + React</h1>
+      <Button>Click me</Button>
+    </main>
+  )
+}
